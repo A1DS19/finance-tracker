425f976349efa45f5cbba32c7fd7fe95f7479004
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2025-03-14 14:21:41 -0700

    month | years params

--- app/routes/_auth/dashboard/transactions/_layout.index.tsx
+++ app/routes/_auth/dashboard/transactions/_layout.index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { z } from "zod";
+import { AllTransactions } from "./-all-transactions";
 
 const today = new Date();
 
@@ -21,8 +22,20 @@ const searchSchema = z.object({
 export const Route = createFileRoute("/_auth/dashboard/transactions/_layout/")({
   component: RouteComponent,
   validateSearch: searchSchema,
+  loaderDeps({ search }) {
+    return {
+      month: search.month ?? today.getMonth() + 1,
+      year: search.year ?? today.getFullYear(),
+    };
+  },
+  loader: async ({ deps }) => {
+    return {
+      ...deps,
+    };
+  },
 });
 
 function RouteComponent() {
-  return <div>Hello "/_auth/dashboard/transactions/"!</div>;
+  const { month, year } = Route.useLoaderDeps();
+  return <AllTransactions month={month} year={year} />;
 }
